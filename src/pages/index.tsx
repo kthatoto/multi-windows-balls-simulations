fa3083439fa0ddcce5670aa79b1b77f42fde56c1
kthatoto <kthatoto@gmail.com>
2023-11-27 02:49:10 +0900

    Fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,10 @@
-import styled from "styled-components";
 import Ball from "@/components/Ball";
 
 const Index = () => {
   return (
-    <Container>
+    <div className="app">
       <Ball />
-    </Container>
+    </div>
   );
 };
 export default Index;
-
-const Container = styled.div`
-  position: relative;
-  width: 100%;
-  height: 100vh;
-  background-color: #eee;
-`;
