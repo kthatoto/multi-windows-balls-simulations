1c72158aaf9ffd966525e8cad7c39df83c5a16b5
kthatoto <kthatoto@gmail.com>
2023-11-29 19:52:30 +0900

    Fix for ssr

--- src/components/Ball.tsx
+++ src/components/Ball.tsx
@@ -1,17 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useInterval } from "@/hooks/useInterval";
 import { moveCircle } from "@/utils/moveCircle";
 
 const Ball = () => {
-  const [radius] = useState(10 + Math.random() * 40);
-  const [position, setPosition] = useState({
-    x: radius + Math.random() * 100,
-    y: radius + Math.random() * 100,
-  });
-  const [velocity, setVelocity] = useState({
-    x: 2 + Math.random() * 4,
-    y: 2 + Math.random() * 4,
-  });
+  const [radius, setRadius] = useState(0);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [velocity, setVelocity] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const _radius = 10 + Math.random() * 40;
+    setRadius(_radius);
+    setPosition({
+      x: _radius + Math.random() * 100,
+      y: _radius + Math.random() * 100,
+    });
+    setVelocity({
+      x: 2 + Math.random() * 4,
+      y: 2 + Math.random() * 4,
+    });
+  }, []);
 
   useInterval(
     () => moveCircle(position, setPosition, velocity, setVelocity, radius),
