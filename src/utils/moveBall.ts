0341e465259dc6d3437222c2927759b8ace45555
kthatoto <kthatoto@gmail.com>
2023-11-30 19:04:59 +0900

    Add

--- src/utils/moveBall.ts
+++ src/utils/moveBall.ts
@@ -5,41 +5,59 @@ const INNER = 0;
 const BORDER_COLLISION = 1;
 const OUTER = -1;
 
+const NO_COLLISION = "no_collision";
+const COLLISION_HORIZONTAL = "collision_horizontal";
+const COLLISION_VERTICAL = "collision_vertical";
+const COLLISION_BOTH = "collision_both";
+
 const windowCollision = (ball: Ball, win: Window) => {
   const winLeft = win.pos.x;
   const winRight = win.pos.x + win.size.width;
   const winTop = win.pos.y;
   const winBottom = win.pos.y + win.size.height;
 
-  const innerHorizontal = winLeft <= ball.pos.x - ball.radius && ball.pos.x + ball.radius <= winRight;
-  const innerVertical = winTop <= ball.pos.y - ball.radius && ball.pos.y + ball.radius <= winBottom;
-  if (innerHorizontal && innerVertical) return INNER;
+  const innerHorizontal = winLeft < ball.pos.x - ball.radius && ball.pos.x + ball.radius < winRight;
+  const innerVertical = winTop < ball.pos.y - ball.radius && ball.pos.y + ball.radius < winBottom;
+  if (innerHorizontal && innerVertical) return { status: INNER, collision: NO_COLLISION };
 
   const outerHorizontal = ball.pos.x + ball.radius < winLeft || winRight < ball.pos.x - ball.radius;
   const outerVertical = ball.pos.y + ball.radius < winTop || winBottom < ball.pos.y - ball.radius;
-  if (outerHorizontal || outerVertical) return OUTER;
+  if (outerHorizontal || outerVertical) return { status: OUTER, collision: NO_COLLISION };
 
-  return BORDER_COLLISION;
+  let collision = NO_COLLISION;
+  if (!innerHorizontal && !innerVertical) {
+    collision = COLLISION_BOTH;
+  } else if (!innerHorizontal) {
+    collision = COLLISION_HORIZONTAL;
+  } else if (!innerVertical) {
+    collision = COLLISION_VERTICAL;
+  }
+
+  return {
+    status: BORDER_COLLISION,
+    collision,
+  };
 };
 
 export const moveBall = (windows: Window[], ball: Ball) => {
   const { pos, velocity } = ball;
   const newBall = {
     ...ball,
-    pos: {x: pos.x + velocity.x, y: pos.y + velocity.y},
+    pos: { x: pos.x + velocity.x, y: pos.y + velocity.y },
   };
+  const newPos = { x: newBall.pos.x, y: newBall.pos.y };
 
   const win = windows.find((w) =>
     w.id === newBall.currentWindowId ||
       (
-        w.pos.x <= pos.x && pos.x <= w.pos.x + w.size.width &&
-        w.pos.y <= pos.y && pos.y <= w.pos.y + w.size.height
+        w.pos.x <= newPos.x && newPos.x <= w.pos.x + w.size.width &&
+        w.pos.y <= newPos.y && newPos.y <= w.pos.y + w.size.height
       )
   )!;
 
   if (!win) return ball;
 
-  const status = windowCollision(newBall, win);
+  const { status } = windowCollision(newBall, win);
   if (status === INNER) {
     return {
       ...newBall,
@@ -47,12 +65,60 @@ export const moveBall = (windows: Window[], ball: Ball) => {
       updatedAt: Date.now(),
     };
   } else if (status === BORDER_COLLISION) {
-    const aroundCollisionWindows = windows.filter((w) => {
+    /* 雑コード */
+    const newWindow = windows.find((w) => {
       if (w.id === win.id) return false;
       if (!win.collisionIds.includes(w.id)) return false;
-      return windowCollision(newBall, w) !== OUTER;
+      return windowCollision(newBall, w).status === BORDER_COLLISION;
     });
+
+    const centerWindow = windows.find((w) =>
+      w.pos.x <= newPos.x && newPos.x <= w.pos.x + w.size.width &&
+      w.pos.y <= newPos.y && newPos.y <= w.pos.y + w.size.height
+    );
+    const currentWindowId = centerWindow?.id;
+
+    if (!newWindow) {
+      return {
+        ...newBall,
+        velocity: {
+          x: -velocity.x,
+          y: velocity.y,
+        },
+        currentWindowId,
+        updatedAt: Date.now(),
+      };
+    }
+    const coll = windowCollision(newBall, newWindow);
+
+    if (coll.collision === COLLISION_BOTH) {
+      return {
+        ...newBall,
+        velocity: {
+          x: -velocity.x,
+          y: -velocity.y,
+        },
+        currentWindowId,
+        updatedAt: Date.now(),
+      };
+    } else if (coll.collision === COLLISION_VERTICAL) {
+      return {
+        ...newBall,
+        velocity: {
+          x: velocity.x,
+          y: -velocity.y,
+        },
+        currentWindowId,
+        updatedAt: Date.now(),
+      };
+    } else if (coll.collision === COLLISION_HORIZONTAL) {
+      return {
+        ...newBall,
+        currentWindowId,
+        updatedAt: Date.now(),
+      };
+    }
   }
-  const otherWindow = windows.find((w) => windowCollision(newBall, w) !== OUTER);
+  const otherWindow = windows.find((w) => windowCollision(newBall, w).status !== OUTER);
   return { ...ball, currentWindowId: otherWindow?.id };
 };
